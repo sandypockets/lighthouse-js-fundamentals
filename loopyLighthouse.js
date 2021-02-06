ec5a198f12743ec89e5b474336afa1c59e47e983
Zac Alexander <zac@znacreative.com>
2021-02-06 17:06:36 +0000

    Fixed some spacing issues

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -1,11 +1,9 @@
 /*
 We will be writing a program that prints the numbers from 100 to 200 to the console, with three exceptions:
-
 If the number is a multiple of 3, print the string "Loopy" instead of the number.
 If the number is a multiple of 4, print the string "Lighthouse" instead of the number.
 If the number is a multiple of both 3 and 4, print the string "LoopyLighthouse" instead of the number.
 */
-
 
 for (let i = 100; i <= 200; i++) {
   if ((i % 3 === 0) && (i % 4 === 0)) {
