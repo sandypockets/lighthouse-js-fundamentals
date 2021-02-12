487bd6ee546d49f650d20422356f6a2937f4ee55
Zac Alexander <zac@znacreative.com>
2021-02-12 02:44:46 +0000

    Fixed returns

--- whichSchool.js
+++ whichSchool.js
@@ -1,18 +1,19 @@
-/*
-Elementary School if age is below 13
-Secondary School if age is between 13 and 18 (both inclusive)
-Lighthouse Labs in all other cases.
-*/
-
 //Function to determine which school the student should attend based on their age
 const whichSchool = function (age) {
   if (age < 13) {
-    console.log('Elementary School');
+    return 'Elementary School';
   } else if (age >= 13 && age <= 18) {
-    console.log('Secondary School');
+    return 'Secondary School';
   } else {
-    console.log('Lighthouse Labs');
+    return 'Lighthouse Labs';
   }
 }
 
-whichSchool(17);
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
